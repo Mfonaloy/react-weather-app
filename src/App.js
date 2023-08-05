d0d8224490e02361f481ee73bc8354f28e567c76
Mfon Aloy <mfonobongsmn82@gmail.com>
2023-08-05 01:49:13 +0100

    Added the Github repo URL.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return <div className="App">
+   <h1> Weather App </h1>
+    <footer>
+      This project was coded by{" "} 
+      <a href="https//www.linkedin.com/in/mfonobong-nnamseh-46529272" target="blank" rel="noopener noreferrer">
+         Mfon Aloy </a>{" "} and is{" "} 
+    <a href="https://github.com/Mfonaloy/react-weather-app"
+    target="blank" rel="noopener noreferrer">open-sourced on Github</a>
+    </footer>
+    </div>;
   
 }
 
