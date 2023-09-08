7daf984c824bc5614d4fea4ff1cf3a4dc2c495b4
Mfon Aloy <mfonobongsmn82@gmail.com>
2023-09-08 21:02:19 +0100

    Edited the weather forecast component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 
 export default function WeatherForecast(props) {
@@ -18,12 +18,7 @@ function handleResponse(response) {
         <div className="WeatherForecast">
          <div className="row">
              <div className="col">
-                 <div className="WeatherForecast-day">Thu</div>
-                 <WeatherIcon code="01d" size={36} />
-                 <div className="WeatherForecast-temperatures">
-                 <span className="WeatherForecast-temperature-max">{forecast[0].temp.max}°</span>
-                 <span className="WeatherForecast-temperature-min">{forecast[0].temp.min}°</span>
-                 </div>
+             <WeatherForecastDay data={forecast[0]}/>
              </div>
              
          </div>
@@ -32,12 +27,13 @@ function handleResponse(response) {
 
 
   } else {
-    let apiKey = "913c3de1a8b304f8eb1cec32eb0eeb51";
+    let apiKey = `7ed26a6948c661d05fafe7355b41b2ec`;
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
- axios.get(apiUrl).then(handleResponse);
+
+    axios.get(apiUrl).then(handleResponse);
 
     return null;
-    }
+  }
 }
