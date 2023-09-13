a8873392da6870b1c3feeb082186f5a115379cc0
Mfon Aloy <mfonobongsmn82@gmail.com>
2023-09-13 18:09:01 +0100

    Refactored codes and warnings fixed.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ export default function App() {
       <a href="https//www.linkedin.com/in/mfonobong-nnamseh-46529272" target="blank" rel="noopener noreferrer">
          Mfon Aloy </a>{" "} and is{" "} 
     <a href="https://github.com/Mfonaloy/react-weather-app"
-    target="blank" rel="noopener noreferrer">open-sourced on Github</a>
+    target="blank" rel="noopener noreferrer">open-sourced on Github</a>{" "} and is{" "}
+    <a href="https://graceful-lollipop-e08a05.netlify.app/" target="blank" rel="noopener noreferrer">
+      hosted on Netlify
+    </a>
     </footer>
     </div>
     </div>;
